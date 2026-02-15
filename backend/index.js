05518dbaa889b707bd4f0daff8cd4661d6d46114
Sarthak <sarthakgupta60788@gmail.com>
2026-02-15 15:29:58 +0530

    project setup

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import dotenv from "dotenv";
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`App is running on Port${PORT}`);
+});
