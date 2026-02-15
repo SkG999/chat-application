24cbef4f2863ce3e47a8cb6f3c3eb8ca758be5a2
Sarthak <sarthakgupta60788@gmail.com>
2026-02-15 17:27:55 +0530

    Complete login, logout Route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import { User } from "../models/userModel.js";
 import bcrypt from "bcryptjs";
-
+import jwt from "jsonwebtoken";
 export const register = async (req, res) => {
   try {
     const { fullname, username, password, confirmPassword, gender } = req.body;
@@ -39,5 +39,63 @@ export const register = async (req, res) => {
     return res
       .status(400)
       .json({ Success: false, message: `Error is ${error}` });
+  }
+};
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res
+        .status(400)
+        .json({ Success: false, message: "Please fill all fields" });
+    }
+    const existUser = await User.findOne({ username });
+    if (!existUser) {
+      return res.status(400).json({
+        Success: false,
+        message: "User not fount with this user name please make account",
+      });
+    }
+    const verifyPassword = await bcrypt.compare(password, existUser.password);
+    if (!verifyPassword) {
+      return res.status(400).json({
+        Success: false,
+        message: "Wrong password !",
+      });
+    }
+    const tokenData = {
+      userId: existUser._id,
+    };
+    const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, {
+      expiresIn: "1d",
+    });
+    //Now store this token in to cookies
+    return res
+      .status(200)
+      .cookie("token", token, {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: "strict",
+      })
+      .json({
+        Success: true,
+        message: "Login successfully",
+        _id: existUser._id,
+        username: existUser.username,
+        fullname: existUser.fullname,
+        profilePhoto: existUser.profilePhoto,
+      });
+  } catch (error) {
+    console.log(`Error is ${error}`);
+  }
+};
+export const logout = async (req, res) => {
+  try {
+    return res
+      .status(200)
+      .cookie("token", "", { maxAge: 0 })
+      .json({ Success: true, message: "Logged Out Successfully !" });
+  } catch (error) {
+    console.log(`Error is : ${error}`);
   }
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
-import { register } from "../controllers/userController.js";
+import { login, logout, register } from "../controllers/userController.js";
 const router = express.Router();
 
 router.post("/register", register);
+router.post("/login", login);
+router.get("/logout", logout);
 export default router;
